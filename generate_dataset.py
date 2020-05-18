e995eda2e1cd3dca7891418592b6ae227255e278
Mikhail <mizolotu@jyu.fi>
2020-05-18 20:17:10 +0300

    18.5.2020

--- generate_dataset.py
+++ generate_dataset.py
@@ -8,12 +8,11 @@ if __name__ == '__main__':
     # test packet extraction using cicids17
 
     dnames, fnames = find_data_files(cicids17pcaps)
-    print(dnames, fnames)
     pcap_fnames = []
     for dname,fname_list in zip(dnames, fnames):
         dfs = [osp.join(dname, fname) for fname in fname_list]
         pcap_fnames.extend([osp.join(cicids17pcaps, df) for df in dfs])
-    print(len(pcap_fnames))
+    print(len(pcap_fnames), pcap_fnames[0])
     features = read_pcap(pcap_fnames[0])
-    print(features[0])
     print(len(features))
+    print(features[0])
