762a4b430afa33c2dc79951ffa1d51ea88c535ae
mikhail <mizolotu@jyu.fi>
2021-03-16 09:33:26 +0200

    16.3.2021

--- plot_progress.py
+++ plot_progress.py
@@ -5,46 +5,45 @@ import plotly.io as pio
 import plotly.graph_objs as go
 
 from common.plot import generate_line_scatter, moving_average
+from config import *
 
 if __name__ == '__main__':
 
-    # args
+    # params
 
-    parser = arp.ArgumentParser(description='Plot results of using state-of-art RL alghorithms in OpenAI gym')
-    parser.add_argument('-i', '--input', help='Input directory', default='results/AttackMitigationEnv/PPO2/MlpPolicy/part2')
-    parser.add_argument('-o', '--output', help='Output directory', default='figures')
+    parser = arp.ArgumentParser(description='Plot progress')
+    parser.add_argument('-i', '--input', help='Input directory', default='results/AttackMitigationEnv/PPO2')
     args = parser.parse_args()
 
-    scenario_name = 'scenario{0}'.format(args.input.split('/')[-1])
-
-    #colors = ['rgb(64,120,211)', 'rgb(0,100,80)', 'rgb(237,2,11)', 'rgb(255,165,0)', 'rgb(139,0,139)', 'rgb(0,51,102)']
-
-    colors = [['rgb(64,120,211)'], ['rgb(237,2,11)']]
+    colors = [['rgb(64,120,211)'], ['rgb(0,100,80)'], ['rgb(237,2,11)'], ['rgb(255,165,0)', 'rgb(139,0,139)', 'rgb(0,51,102)']]
 
     fname = osp.join(args.input, 'progress.csv')
     p = pandas.read_csv(fname, delimiter=',', dtype=float)
-    y = p['ep_reward_mean'].values
+    r = p['ep_reward_mean'].values
     n = p['ep_normal_mean'].values
     a = p['ep_attack_mean'].values
     x = p['total_timesteps'].values
-    y = moving_average(y.reshape(len(y), 1)).reshape(x.shape)
+    r = moving_average(r.reshape(len(r), 1)).reshape(x.shape)
     n = moving_average(n.reshape(len(n), 1)).reshape(x.shape)
     a = moving_average(a.reshape(len(a), 1)).reshape(x.shape)
 
-    data = [[[x, y]], [[x, a]]]
-    names = [['Reward'], ['Malicious traffic']]
-    fnames = ['reward', 'traffic']
-    ylabels = ['Reward value', 'Malicious traffic']
+    data = [[[x, r]], [[x, n]], [[x, a]]]
+    names = [['Reward'], ['Benign traffic'], ['Malicious traffic']]
+    fnames = ['reward', 'benign', 'malicious']
+    ylabels = ['Reward value', 'Benign traffic', 'Malicious traffic']
 
-    for d, n, c, f, y in zip(data, names, colors, fnames, ylabels):
-        traces, layout = generate_line_scatter(n, d, c, 'Time steps', y, show_legend=False)
+    for d, n, f, y, c in zip(data, names, fnames, ylabels, colors):
+
+        # generate scatter
+
+        traces, layout = generate_line_scatter(n, d, c, 'Time steps', y, show_legend=True, xrange=[0, 463872])
 
         # save results
 
         ftypes = ['png', 'pdf']
-        if not osp.exists(args.output):
-            os.mkdir(args.output)
-        fig_fname = '{0}/{1}'.format(args.output, f)
+        if not osp.exists(progress_dir):
+            os.mkdir(progress_dir)
+        fig_fname = '{0}/{1}'.format(progress_dir, f)
         fig = go.Figure(data=traces, layout=layout)
         for ftype in ftypes:
             pio.write_image(fig, '{0}.{1}'.format(fig_fname, ftype))
