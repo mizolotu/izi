84cf5cb50fe252858895eb7653aefd662f809e60
mizolotu <mizolotu@jyu.fi>
2021-03-30 17:49:43 +0300

    30.3.2021

--- scripts/ovs.sh
+++ scripts/ovs.sh
@@ -1,7 +1,6 @@
 sudo apt update -y
 sudo apt install openvswitch-switch -y
 sudo ovs-vsctl add-br br
-#sudo ovs-vsctl add-port br eth2
-#sudo ifconfig eth2 0
 sudo ovs-vsctl set-controller br tcp:192.168.254.11:6653
 sudo ovs-ofctl del-flows br
+sudo ovs-vsctl -- --id=@sflow create sflow agent=eth1 target="\"192.168.254.1:6343\"" header=128 polling=1 sampling=1 -- set bridge br sflow=@sflow
