ffe0e36eb5e8ba8294c03103de32cd17b064460f
mizolotu <mizolotu@jyu.fi>
2021-04-26 12:13:41 +0300

    26.4.2021

--- reinforcement_learning/gym/envs/reactive1/main.py
+++ reinforcement_learning/gym/envs/reactive1/main.py
@@ -63,6 +63,7 @@ class AttackMitigationEnv():
         ovs_vms = [vm for vm in self.vms if vm['role'] == 'ovs' and int(vm['vm'].split('_')[1]) == self.id]
         assert len(ovs_vms) == 1
         self.ovs_vm = ovs_vms[0]
+        print(self.id, self.ovs_vm)
         self.ovs_node = self.nodes[self.ovs_vm['vm']]
         set_seed(self.ovs_vm['mgmt'], flask_port, self.seed)
 
