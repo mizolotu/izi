24a93ccd90e2816f14eb2eedc2959c6a498b698b
mizolotu <mizolotu@jyu.fi>
2021-06-14 11:12:46 +0300

    14.6.2021

--- reinforcement_learning/gym/envs/reactive1/main.py
+++ reinforcement_learning/gym/envs/reactive1/main.py
@@ -537,6 +537,7 @@ class AttackMitigationEnv():
 
         for host in self.internal_hosts:
             _ = replay_ip_traffic_on_interface(self.ovs_vm['mgmt'], flask_port, host, self.label, episode_duration, aug=self.aug)
+            print(_)
 
         self.tstart = time()
         self.tstep = time()
