f4116e101866ac6fad48e8d7bba7e79e9cce91fb
mizolotu <mizolotu@jyu.fi>
2021-06-14 10:18:13 +0300

    14.6.2021

--- reinforcement_learning/ppo2/ppo2.py
+++ reinforcement_learning/ppo2/ppo2.py
@@ -534,9 +534,9 @@ class Runner(AbstractEnvRunner):
             #self.mb_rewards[env_idx].append(rewards)
 
         print(f'Step time in {env_idx}: {(time.time() - tstart) / self.n_steps}')
-        print(infos)
 
         self.mb_rewards[env_idx], infos = self.env.reward_one(env_idx)
+        print(infos)
         self.scores[env_idx] = [[info['r'], info['n'], info['a'], info['p']] for info in infos]
 
     def _run(self):
