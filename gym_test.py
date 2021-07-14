20c0368048aa7de797a573edb7398944ec58f662
mizolotu <mizolotu@jyu.fi>
2021-07-14 10:05:01 +0300

    14.7.2021

--- gym_test.py
+++ gym_test.py
@@ -2,7 +2,7 @@ from reinforcement_learning.gym.envs.classic_control.cartpole import CartPoleEnv
 from reinforcement_learning.gym.envs.box2d.lunar_lander import LunarLander
 from reinforcement_learning.common.vec_env import SubprocVecEnv
 from reinforcement_learning.common.policies import MlpPolicy, ICMPolicy
-from reinforcement_learning.ppo2.ppo2_curiosity import PPO2 as ppo_c
+from reinforcement_learning.ppoc.ppoc import PPOC as ppo_c
 from reinforcement_learning.ppo2.ppo2 import PPO2 as ppo
 from reinforcement_learning.acktr.acktr import ACKTR as acktr
 from reinforcement_learning.acer.acer_simple import ACER as acer
@@ -17,25 +17,22 @@ if __name__ == '__main__':
     env_class = LunarLander
     #env_class = CartPoleEnv
     nenvs = 4
-    nsteps = nenvs * int(10e6)
+    nsteps = nenvs * int(9e6)
 
     set_global_seeds(seed=0)
     env_fns = [make_env(env_class, seed) for seed in range(nenvs)]
     env = SubprocVecEnv(env_fns)
 
-    #model = ppo(MlpPolicy, env, n_steps=512, seed=0, verbose=1)
-    #model.learn(total_timesteps=nsteps)
-
-    #model = ppo_c(MlpPolicy, env, n_steps=512, seed=0, verbose=1)
-    #model.learn(total_timesteps=nsteps)
-
-    #model = ppo_c(MlpPolicy, env, n_steps=512, seed=0, verbose=1, int_coef=0.1)
-    #model.learn(total_timesteps=nsteps)
-
-    model = ppo_c(MlpPolicy, env, n_steps=512, seed=0, verbose=1, int_coef=0.01)
+    model = ppo(MlpPolicy, env, n_steps=512, seed=0, verbose=1)
     model.learn(total_timesteps=nsteps)
 
-    model = ppo_c(MlpPolicy, env, n_steps=512, seed=0, verbose=1, int_coef=0.001)
+    model = ppo_c(MlpPolicy, env, n_steps=512, seed=0, verbose=1)
+    model.learn(total_timesteps=nsteps)
+
+    model = ppo_c(MlpPolicy, env, n_steps=512, seed=0, verbose=1, lmd=0.1)
+    model.learn(total_timesteps=nsteps)
+
+    model = ppo_c(MlpPolicy, env, n_steps=512, seed=0, verbose=1, lmd=0.01)
     model.learn(total_timesteps=nsteps)
 
     #model = ppo_c(MlpPolicy, env, n_steps=512, seed=0, verbose=1)
