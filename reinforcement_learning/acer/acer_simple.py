f652966d72a825341b79ba185a59a047a6155a11
mikhail <mizolotu@jyu.fi>
2021-08-01 21:34:05 +0300

    1.8.2021

--- reinforcement_learning/acer/acer_simple.py
+++ reinforcement_learning/acer/acer_simple.py
@@ -798,7 +798,9 @@ class _Runner(AbstractEnvRunner):
 
         ep_infos = []
 
-        self.obs[:] = self.env.reset()
+        tmp = self.env.reset()
+        print(tmp)
+        self.obs[:] = tmp
 
         while np.isnan(np.sum(self.obs)):
             controller = self.env.get_attr('controller', [0])[0]
